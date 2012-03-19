515e16d355bafb8313a432c0258e17bf2d2cec67
Pew <pew@eve.local>
2012-03-19 17:44:31 -0400

    more merge foolishness

---
+++ garth-ios/garth/SettingsViewController.h
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.h
+//  garth
+//
+//  Created by Pew on 12-03-19.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController <UITextFieldDelegate> {
+    UITextField *serverIPTextField_;
+    UIButton *connectButton_;
+}
+
+
+
+@end
