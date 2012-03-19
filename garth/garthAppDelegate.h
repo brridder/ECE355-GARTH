04de9026be060d5597dbb459079b78c8ad4db873
Pew <pew@eve.local>
2012-03-19 15:25:15 -0400

    Initial Commit

---
+++ garth/garthAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  garthAppDelegate.h
+//  garth
+//
+//  Created by Pew on 12-03-19.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface garthAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ garth/iPhone/garthAppDelegate_iPhone.m
@@ -0,0 +1,13 @@
+//
+//  garthAppDelegate_iPhone.m
+//  garth
+//
+//  Created by Pew on 12-03-19.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "garthAppDelegate_iPhone.h"
+
+@implementation garthAppDelegate_iPhone
+
+@end
